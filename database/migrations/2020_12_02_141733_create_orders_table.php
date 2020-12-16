69e19f38340210edf1412e882e2b16112a9672bc
YsolineG <ysoline.ganster@gmail.com>
2020-12-16 11:52:20 +0100

    refactor:

--- database/migrations/2020_12_02_141733_create_orders_table.php
+++ database/migrations/2020_12_02_141733_create_orders_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// @codingStandardsIgnoreLine
 class CreateOrdersTable extends Migration
 {
     /**
@@ -28,6 +29,5 @@ class CreateOrdersTable extends Migration
     public function down()
     {
         Schema::dropIfExists('orders');
-        
     }
 }
